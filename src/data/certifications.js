2cfb772bd6bc040211bcaa77d2435be553000c6a
tinsarirauhana <tinsari.r@mhs.usk.ac.id>
2025-12-20 18:18:05 +0700

    adding new certificates

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,8 +1,8 @@
 const certifications = [
   { title: "Data Mining Teaching Assistant", issuer: "Universitas Syiah Kuala", date: "2025", image: "/certificates/cert1.jpg" },
   { title: "National UI/UX Design Competition", issuer: "1st Place Winner", date: "2025", image: "/api/placeholder/400/300" },
+  { title: "Gelar Rasa Data Science Competition", issuer: "National Finalist", date: "2025", image: "/certificates/cert5.png" },
   { title: "National UI/UX Design Competition", issuer: "National Finalist", date: "2025", image: "/certificates/cert2.png" },
-  { title: "National Data Competition", issuer: "National Finalist", date: "2025", image: "/api/placeholder/400/300" },
   { title: "Student Organization", issuer: "Branding & Creative Communication", date: "2024-2025", image: "/api/placeholder/400/300" },
   {title: "Best Practices for UI/UX Beginners", issuer: "Dilesin Academy", date: "2025", image: "/certificates/cert3.jpg"},
   {title: "Back-End Development and APIs (V8)", issuer: "freeCodeCamp", date: "2025", image: "/certificates/cert4.png"}
