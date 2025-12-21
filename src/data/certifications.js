a908628e7faabb7e9d87f229bb4db9fd20061af4
tinsarirauhana <tinsari.r@mhs.usk.ac.id>
2025-12-21 19:05:39 +0700

    update certificate

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -5,8 +5,9 @@ const certifications = [
   { title: "National UI/UX Design Competition", issuer: "National Finalist", date: "2025", image: "/certificates/cert2.png" },
   { title: "Student Organization", issuer: "Branding & Creative Communication", date: "2024-2025", image: "/api/placeholder/400/300" },
   {title: "Best Practices for UI/UX Beginners", issuer: "Dilesin Academy", date: "2025", image: "/certificates/cert3.jpg"},
-  {title: "Back-End Development and APIs (V8)", issuer: "freeCodeCamp", date: "2025", image: "/certificates/cert4.png"}
-  
+  {title: "Back-End Development and APIs (V8)", issuer: "freeCodeCamp", date: "2025", image: "/certificates/cert4.png"},
+  {title: "AI Innovation Training", issuer: "Plan International Indonesia & KEMENPORA", date: "2025", image: "/certificates/cert6.jpg"}
+
 
 ];
 
