10172d2f2468fb9983910dfdc2c7a1eaf71232c2
tinsarirauhana <tinsari.r@mhs.usk.ac.id>
2026-01-28 12:23:07 +0700

    adding new certificates

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,14 +1,14 @@
 const certifications = [
   { title: "Data Mining Teaching Assistant", issuer: "Universitas Syiah Kuala", date: "2025", image: "/certificates/cert1.jpg" },
-  { title: "National UI/UX Design Competition", issuer: "1st Place Winner", date: "2025", image: "/api/placeholder/400/300" },
+  { title: "National UI/UX Design Competition", issuer: "1st Place Winner", date: "2025", image: "/certificates/cert9.png" },
   { title: "Gelar Rasa Data Science Competition", issuer: "National Finalist", date: "2025", image: "/certificates/cert5.png" },
   { title: "National UI/UX Design Competition", issuer: "National Finalist", date: "2025", image: "/certificates/cert2.png" },
-  { title: "Student Organization", issuer: "Branding & Creative Communication", date: "2024-2025", image: "/api/placeholder/400/300" },
-  {title: "Best Practices for UI/UX Beginners", issuer: "Dilesin Academy", date: "2025", image: "/certificates/cert3.jpg"},
+  //{ title: "Student Organization", issuer: "Branding & Creative Communication", date: "2024-2025", image: "/api/placeholder/400/300" },
   {title: "Back-End Development and APIs (V8)", issuer: "freeCodeCamp", date: "2025", image: "/certificates/cert4.png"},
   {title: "AI Innovation Training", issuer: "Plan International Indonesia & KEMENPORA", date: "2025", image: "/certificates/cert6.jpg"},
   {title: "Data Analytics: SQL (Online Course)", issuer: "Ioda Academy", date: "2025", image: "/certificates/cert7.png"},
-  {title: "Guide to Learn R with AI (Online Course)", issuer: "DQLab", date: "2025", image: "/certificates/cert8.png"}
+  {title: "Guide to Learn R with AI (Online Course)", issuer: "DQLab", date: "2025", image: "/certificates/cert8.png"},
+  {title: "Best Practices for UI/UX Beginners", issuer: "Dilesin Academy", date: "2025", image: "/certificates/cert3.jpg"}
 
 
 
